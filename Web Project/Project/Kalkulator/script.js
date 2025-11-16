1b500cb728bd7d32117828369b5246fc0f8e0a7a
R4mFaaaw <rafamirzafirdaus@gmail.com>
2025-11-16 20:54:03 +0700

    Buat Kalkulator sederhana #2

---
+++ Web Project/Project/Kalkulator/script.js
@@ -0,0 +1,7 @@
+let btn9 = document.getElementById("9");
+let btn8 = document.getElementById("8");
+let btn7 = document.getElementById("7");
+let output = document.getElementById("Output");
+
+output = btn9.onclick().function()
+    output = 9;
